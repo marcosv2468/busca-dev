a669ce0b01c9a6ce34797c15e55d8192f3d43d9d
marcosv2468 <marcosv2468@gmail.com>
2020-01-16 19:47:52 -0300

    Layout web finalizado

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,62 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  
-  function incrementCounter() {
-    setCounter(counter + 1)
-  }
-  
+
   return (
-    <>
-      <h1>Contador: { counter }</h1>
-      <button onClick={ incrementCounter }>Incrementar</button> 
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header> 
+              <img src="https://avatars3.githubusercontent.com/u/28361577?s=460&v=4" alt="Marcos Vinícius"/>
+              <div className="user-info">
+                <strong>Marcos Vinícius</strong>
+                <span>ReactJS, NodeJS e React Native</span>
+              </div>
+            </header>
+            <p>
+              Formado no curso Técnico em Informática - IFPI - em 2019. 
+              Atualmente estudante de Análise e Desenvolvimento de Sistemas - IFPI.
+            </p>
+            <a href="https://github.com/marcosv2468">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+
   );
 }
 
